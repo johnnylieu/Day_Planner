5eb2b6eae3a7df0e085720b0792e5ec3e5288f12
Johnny Lieu <johnnylieu@yahoo.com>
2020-08-02 23:06:16 -0700

    revmoed time display

--- moment.js
+++ moment.js
@@ -1,8 +1,19 @@
 // displays the current date and time
 $(document).ready(function() {
-    $("#currentDay").text(moment().format('ddd MMM Do, YYYY, h:mm:ss a'));
+    $("#currentDay").text(moment().format('ddd MMM Do, YYYY'));
 });
 
+// variables in case i need them, ended up not having to use any
+var currentTime = $("#currentDay");
+var nine = $("#9");
+var ten = $("#description10");
+var eleven = $("#description11");
+var twelve = $("#description12");
+var one = $("#description13");
+var two = $("#description14");
+var three = $("#description15");
+var four = $("#description16");
+var five = $("#description17");
 
 function hourUpdate() {
     var currentHour = moment().hour(); // updates hour every 1.5 secs
@@ -25,6 +36,7 @@ function hourUpdate() {
             $(this).attr("class", "present"); // present
         }
     });
+    
 }
 
-var interval = setInterval(hourUpdate, 1500); // runs hourUpdate every 1.5 seconds
+var interval = setInterval(hourUpdate, 1500); // every 1.5 seconds
