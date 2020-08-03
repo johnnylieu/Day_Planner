47ef915d2fc6711b648ee01f4099bcf13a82edee
Johnny Lieu <johnnylieu@yahoo.com>
2020-08-02 22:27:41 -0700

    deleted unneeded vars

--- moment.js
+++ moment.js
@@ -3,17 +3,6 @@ $(document).ready(function() {
     $("#currentDay").text(moment().format('ddd MMM Do, YYYY, h:mm:ss a'));
 });
 
-// variables in case i need them, ended up not having to use any
-var currentTime = $("#currentDay");
-var nine = $("#9");
-var ten = $("#description10");
-var eleven = $("#description11");
-var twelve = $("#description12");
-var one = $("#description13");
-var two = $("#description14");
-var three = $("#description15");
-var four = $("#description16");
-var five = $("#description17");
 
 function hourUpdate() {
     var currentHour = moment().hour(); // updates hour every 1.5 secs
@@ -36,7 +25,6 @@ function hourUpdate() {
             $(this).attr("class", "present"); // present
         }
     });
-    
 }
 
 var interval = setInterval(hourUpdate, 1500); // runs hourUpdate every 1.5 seconds
